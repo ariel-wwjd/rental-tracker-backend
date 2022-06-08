dcad4137609e92281b7d984b286cae64438e83a6
Ariel Arias <ariel.arias@encora.com>
2022-06-08 18:09:26 -0400

    update on the login proccess

--- app.js
+++ app.js
@@ -1,40 +1,19 @@
 require('dotenv').config({ path: './src/config/config.env' });
 const express = require('express');
 const morgan = require('morgan');
-const passport = require('passport');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const cookieSession = require('cookie-session');
 const cors = require('cors');
 const routes = require('./src/routes');
 require('./src/database/mongo')();
-require('./src/config/passport')(passport);
 
 const app = express();
 const { PORT, CLIENT_URL } = process.env;
-const oneDayMilliseconds = 24 * 60 * 60 * 1000;
-const cookieKey = process.env.COOKIE_KEY;
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+app.use(express.json());
 app.use(cors({ origin: CLIENT_URL, credentials: true }));
-app.use(cookieSession({
-  maxAge: oneDayMilliseconds,
-  keys: [cookieKey],
-}));
-
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false,
-  store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
-}));
-
-app.use(passport.initialize());
-
-app.use(passport.session());
 
 app.use('/api', routes);
 
